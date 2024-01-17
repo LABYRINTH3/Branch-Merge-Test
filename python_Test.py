d2f63ff6b6610abe22c8db0e42c114223f2c6f47
LTH <graphene03@gmail.com>
2024-01-17 16:39:08 +0900

    test2 branch

--- python_Test.py
+++ python_Test.py
@@ -1,4 +1,16 @@
 import random
 
-print(random.choice([1,2,3]))
+print(random.choice([1, 2, 3]))
 print("HEllO")
+
+# 두 숫자를 더하고 곱하는 간단한 계산
+num1 = 5
+num2 = 7
+
+# 덧셈
+sum_result = num1 + num2
+print(f"덧셈 결과: {sum_result}")
+
+# 곱셈
+multiply_result = num1 * num2
+print(f"곱셈 결과: {multiply_result}")
